e9f690b3462178b7903f08871d255b726c26b4e3
Mohamed Ali <mohamedalibk66@gmail.com>
2025-03-01 21:49:10 +0200

    commit

--- HotelSystem/Data/Context.cs
+++ HotelSystem/Data/Context.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using HotelSystem.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace HotelSystem.Data
@@ -16,6 +17,35 @@ namespace HotelSystem.Data
                 .LogTo(log => Debug.WriteLine(log), LogLevel.Information)
                 .EnableSensitiveDataLogging();
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RoomFacility>()
+                .HasKey(rf => new { rf.RoomId, rf.FacilityId });
+
+            modelBuilder.Entity<RoomFacility>()
+                .HasOne(rf => rf.Room)
+                .WithMany(r => r.RoomFacilities)
+                .HasForeignKey(rf => rf.RoomId);
+
+            modelBuilder.Entity<RoomFacility>()
+            .HasOne(rf => rf.Facility)
+            .WithMany(f => f.RoomFacilities)
+            .HasForeignKey(rf => rf.FacilityId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
+         
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomType> RoomTypes { get; set; }
+        public DbSet<Facility> Facilities { get; set; }
+        public DbSet<RoomFacility> RoomFacilities { get; set; }
+
+
+
 
     }
 }
